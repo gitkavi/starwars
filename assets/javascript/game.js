1aca6da91e6b333559db4d5c1f896a53c5678aca
kavitha <kavitha.veluchamy@gmail.com>
2018-03-19 22:33:08 -0400

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var hp1 = 110;
+    var ap1 = 8;
+    var cap1 = 0;
+    var hp2 = 150;
+    var ap2 = 5;
+    var cap2 = 0;
+    var hp3 = 200;
+    var ap3 = 10;
+    var cap3 = 0;
+    var hp4 = 180;
+    var ap4 = 9;
+    var cap4 = 0;
+
+    $("#player1-hp").html(hp1);
+    $("#player2-hp").html(hp2);
+    $("#player3-hp").html(hp3);
+    $("#player4-hp").html(hp4);
+});
